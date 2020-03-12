58401524c9de252f9802d2514b1c0919453ade7c
Abhishek Bhattacharya <abhishekbhattacharya@Abhisheks-MacBook-Pro.local>
2020-03-13 00:13:22 +0530

    For Loop in React HTML

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,23 @@
 import React from 'react';
+//import { robots } from './robo';
 
-const Card = ({id,name,email}) => {
-    return(
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/test/${id}`}/>
-            <div>
-                <h2>{id}</h2>
-                <p>{name}</p>
-                <p>{email}</p>
+const Card = ({robot}) => {
+    
+    const cardArr = robot.map((arr,i)=>{
+        return(
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' key={robot[i].id}>
+                <img alt='robots' src={`https://robohash.org/test/${i}`}/>
+                <div>
+                    <h2>{robot[i].id}</h2>
+                    <p>{robot[i].name}</p>
+                    <p>{robot[i].email}</p>
+                </div>
             </div>
-        </div>
-    );
+        )
+    })
+    return(
+        <div>{cardArr}</div>
+    )
 }
 
 export default Card;
--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import {robots} from './robo';
 import 'tachyons';
 
 ReactDOM.render(<div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+        {/* <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/> */
+        <Card robot = {robots}></Card>
+        }
+        
     </div>,
      document.getElementById('root'));
 
