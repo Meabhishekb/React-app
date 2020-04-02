d1a8c6fc1d56d2aff837ef0dd9787d26ae2ace5e
Abhishek Bhattacharya <abhishekbhattacharya@Abhisheks-MacBook-Pro.local>
2020-04-02 23:23:35 +0530

    Updates

---
+++ precache-manifest.78311585446c7260f18c4d625ac5c391.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f498e15c904e6b98988c5b26c7ebe043",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "b6f4eca2813271b0ea2d",
+    "url": "/React-app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "ef819eeec1040217fa1c",
+    "url": "/React-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b6f4eca2813271b0ea2d",
+    "url": "/React-app/static/js/2.42c2de0d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-app/static/js/2.42c2de0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef819eeec1040217fa1c",
+    "url": "/React-app/static/js/main.b7157994.chunk.js"
+  },
+  {
+    "revision": "c91be0fb6ff3a2ccecbd",
+    "url": "/React-app/static/js/runtime-main.1e79d9ac.js"
+  }
+]);
